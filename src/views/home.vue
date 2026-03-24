96de7a860b34678d1c689d13e2838dff10047040
orbitai776 <orbitai776>
2026-03-24 09:31:59 +0700

    Update hook author

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,7 +2,7 @@
   <div class="h-screen bg-gradient-to-r from-primary to-secondary flex items-center justify-center">
     <div class="text-center text-white">
       <h1 class="text-5xl font-bold mb-4">Orbit AI</h1>
-      <p class="text-xl">Welcome to Orbit AI, env : {{ env }}</p>
+      <p class="text-xl">Welcome to Orbit AI, env-secret : {{ env }}</p>
     </div>
   </div>
 </template>
