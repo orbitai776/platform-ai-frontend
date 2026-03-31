56fb66eafa67c8a012639a14c36bbbc0c5013c83
phanvu2004 <90810485+phanvu2004@users.noreply.github.com>
2026-03-31 13:28:06 +0700

    Update navbar.vue

--- src/views/navbar.vue
+++ src/views/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="bg-blue-100 text-black shadow-md">
     <div class="container mx-auto px-6 py-4 flex justify-between items-center">
-      <div class="text-2xl font-bold">Platform test more and more</div>
+      <div class="text-2xl font-bold">Platform test pipeline</div>
       <ul class="flex space-x-6">
         <li>
           <a href="/" class="hover:text-accent transition">Home</a>
